ffdfb6ccd97701693a2676a559d3c430cb000290
OtisHuang-new <khoihoang862006@gmail.com>
2025-12-24 09:54:05 +0700

    adjust

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 
 function App() {
+    const [isLoading, setIsLoading] = useState(false);
     const [content, setContent] = useState("");
     const [startDate, setStartDate] = useState("2025-01-01");
     const [endDate, setEndDate] = useState("2025-01-01");
@@ -11,8 +12,7 @@ function App() {
     
     const handleSplit = () => {
         if (!content || typeof content.split !== 'function') {
-            alert("Dữ liệu không đúng chuẩn");
-            return;
+            throw new Error("Dữ liệu không đúng chuẩn");
           }
         
         const lines = content?.split('\n') || [];
@@ -24,11 +24,9 @@ function App() {
             }
         }
         if(startIdx === -1){
-            alert("Dữ liệu không đúng chuẩn");
-            return;
+            throw new Error("Dữ liệu không đúng chuẩn");
         }else if(startIdx === lines.length - 1){
-            alert("Không có Dữ liệu lịch thi");
-            return;
+            throw new Error("Không có Dữ liệu lịch thi");
         }        
         startIdx++;
 
@@ -55,75 +53,80 @@ function App() {
         return {'lines': lines, 'startIdx': startIdx};
     }
     const createCalendarToFile = () => {
-        let {lines, startIdx} = handleSplit();
-        
-        let myCalendar = `BEGIN:VCALENDAR\nVERSION:2.0\nPRODID:-//MyReactApp//NONSGML v1.0//EN\nCALSCALE:GREGORIAN\nMETHOD:PUBLISH\nX-WR-CALNAME:${"Thời khóa biểu " + lines[startIdx - 4]}\nX-WR-TIMEZONE:Asia/Ho_Chi_Minh\n`;
-        const fileName = `UScheduled_${lines[startIdx - 4]}`;
+        try{
+            const splited = handleSplit();
+            const lines = splited.lines;
+            let startIdx = splited.startIdx;
+            let myCalendar = `BEGIN:VCALENDAR\nVERSION:2.0\nPRODID:-//MyReactApp//NONSGML v1.0//EN\nCALSCALE:GREGORIAN\nMETHOD:PUBLISH\nX-WR-CALNAME:${"Thời khóa biểu " + lines[startIdx - 4]}\nX-WR-TIMEZONE:Asia/Ho_Chi_Minh\n`;
+            const fileName = `UScheduled_${lines[startIdx - 4]}`;
 
-        while(lines[startIdx] !== end_line && startIdx < lines.length){
-            const line = lines[startIdx].split('\t');
+            while(lines[startIdx] !== end_line && startIdx < lines.length){
+                const line = lines[startIdx].split('\t');
 
 
-            const myEvent   = `BEGIN:VEVENT\n`
-                            + `DTSTAMP:${new Date().toISOString().replace(/[-:]/g, "").split(".")[0] + "Z"}\n`
-                            + `LOCATION:\n`
-                            + `STATUS:CONFIRMED\n`;
+                const myEvent   = `BEGIN:VEVENT\n`
+                                + `DTSTAMP:${new Date().toISOString().replace(/[-:]/g, "").split(".")[0] + "Z"}\n`
+                                + `LOCATION:\n`
+                                + `STATUS:CONFIRMED\n`;
 
-            let theory = line[9].split(" ");
-            let experiment = line[10].split(" ");
-            if(theory[0] !== ""){
-                const st = new Date(startDate);
-                while (st.getDay() !== convert[theory[0]]) { 
-                    st.setDate(st.getDate() + 1);
+                let theory = line[9].split(" ");
+                let experiment = line[10].split(" ");
+                if(theory[0] !== ""){
+                    const st = new Date(startDate);
+                    while (st.getDay() !== convert[theory[0]]) { 
+                        st.setDate(st.getDate() + 1);
+                    }
+                    // console.log(st.toISOString().split("T")[0]);
+                    let event = myEvent;
+                    event   +=`UID:LT${line[0] + line[1]}@myCalendar.com\n`
+                            +`SUMMARY:${line[2]} (Lý thuyết)\n`
+                            +`DESCRIPTION:Mã môn học: ${line[1]}\\nSố tín chỉ: ${line[3]}\\nPhòng: ${theory[2].replace(/[()]/g, "")}\\n`
+                            +`GV:${line[11]}\\nGVTG:${((line[13].length !== 0) ? (line[13]) : "")}\n`
+                            +`DTSTART:${new Date(`${st.toISOString().split("T")[0]}T${theory[1].split("-")[0]}:00+07:00`).toISOString().replace(/[-:]/g, "").split(".")[0] + "Z"}\n`
+                            +`DTEND:${new Date(`${st.toISOString().split("T")[0]}T${theory[1].split("-")[1]}:00+07:00`).toISOString().replace(/[-:]/g, "").split(".")[0] + "Z"}\n`
+                            +`RRULE:FREQ=WEEKLY;UNTIL=${new Date(`${endDate}T23:59:00+07:00`).toISOString().replace(/[-:]/g, "").split(".")[0] + "Z"};BYDAY=${mapping[theory[0]] || "MO"}\n`
+                            +`END:VEVENT\n`;
+                    // console.log(event);
+                    myCalendar += event;
                 }
-                console.log(st.toISOString().split("T")[0]);
-                let event = myEvent;
-                event   +=`UID:LT${line[0] + line[1]}@myCalendar.com\n`
-                        +`SUMMARY:${line[2]} (Lý thuyết)\n`
-                        +`DESCRIPTION:Mã môn học: ${line[1]}\\nSố tín chỉ: ${line[3]}\\nPhòng: ${theory[2].replace(/[()]/g, "")}\\n`
-                        +`GV:${line[11]}\\nGVTG:${((line[13].length !== 0) ? (line[13]) : "")}\n`
-                        +`DTSTART:${new Date(`${st.toISOString().split("T")[0]}T${theory[1].split("-")[0]}:00+07:00`).toISOString().replace(/[-:]/g, "").split(".")[0] + "Z"}\n`
-                        +`DTEND:${new Date(`${st.toISOString().split("T")[0]}T${theory[1].split("-")[1]}:00+07:00`).toISOString().replace(/[-:]/g, "").split(".")[0] + "Z"}\n`
-                        +`RRULE:FREQ=WEEKLY;UNTIL=${new Date(`${endDate}T23:59:00+07:00`).toISOString().replace(/[-:]/g, "").split(".")[0] + "Z"};BYDAY=${mapping[theory[0]] || "MO"}\n`
-                        +`END:VEVENT\n`;
-                // console.log(event);
-                myCalendar += event;
-            }
 
-            if(experiment[0] !== ""){
-                const st = new Date(startDate);
-                while (st.getDay() !== convert[experiment[0]]) { 
-                    st.setDate(st.getDate() + 1);
+                if(experiment[0] !== ""){
+                    const st = new Date(startDate);
+                    while (st.getDay() !== convert[experiment[0]]) { 
+                        st.setDate(st.getDate() + 1);
+                    }
+                    st.setDate(st.getDate() + 7);
+                    let event = myEvent;
+                    event   +=`UID:TH${line[0] + line[1]}@myCalendar.com\n`
+                            +`SUMMARY:${line[2]} (Thực Hành)\n`
+                            +`DESCRIPTION:Mã môn học: ${line[1]}\\nSố tín chỉ: ${line[3]}\\nPhòng: ${experiment[2].replace(/[()]/g, "")}\\n`
+                            +`GV:${line[12]}\\nGVTG:${((line[13].length !== 0) ? (line[13]) : "")}\n`
+                            +`DTSTART:${new Date(`${st.toISOString().split("T")[0]}T${experiment[1].split("-")[0]}:00+07:00`).toISOString().replace(/[-:]/g, "").split(".")[0] + "Z"}\n`
+                            +`DTEND:${new Date(`${st.toISOString().split("T")[0]}T${experiment[1].split("-")[1]}:00+07:00`).toISOString().replace(/[-:]/g, "").split(".")[0] + "Z"}\n`
+                            +`EXDATE:${startDate.replace(/-/g,"")}\n`
+                            +`RRULE:FREQ=WEEKLY;UNTIL=${new Date(`${endDate}T23:59:00+07:00`).toISOString().replace(/[-:]/g, "").split(".")[0] + "Z"};BYDAY=${mapping[experiment[0]] || "MO"}\n`
+                            +`END:VEVENT\n`;
+                    // console.log(event);
+                    myCalendar += event;
                 }
-                st.setDate(st.getDate() + 7);
-                let event = myEvent;
-                event   +=`UID:TH${line[0] + line[1]}@myCalendar.com\n`
-                        +`SUMMARY:${line[2]} (Thực Hành)\n`
-                        +`DESCRIPTION:Mã môn học: ${line[1]}\\nSố tín chỉ: ${line[3]}\\nPhòng: ${experiment[2].replace(/[()]/g, "")}\\n`
-                        +`GV:${line[12]}\\nGVTG:${((line[13].length !== 0) ? (line[13]) : "")}\n`
-                        +`DTSTART:${new Date(`${st.toISOString().split("T")[0]}T${experiment[1].split("-")[0]}:00+07:00`).toISOString().replace(/[-:]/g, "").split(".")[0] + "Z"}\n`
-                        +`DTEND:${new Date(`${st.toISOString().split("T")[0]}T${experiment[1].split("-")[1]}:00+07:00`).toISOString().replace(/[-:]/g, "").split(".")[0] + "Z"}\n`
-                        +`EXDATE:${startDate.replace(/-/g,"")}\n`
-                        +`RRULE:FREQ=WEEKLY;UNTIL=${new Date(`${endDate}T23:59:00+07:00`).toISOString().replace(/[-:]/g, "").split(".")[0] + "Z"};BYDAY=${mapping[experiment[0]] || "MO"}\n`
-                        +`END:VEVENT\n`;
-                // console.log(event);
-                myCalendar += event;
+                startIdx++;
             }
-            startIdx++;
-        }
-        myCalendar += "END:VCALENDAR";
-        // console.log(myCalendar);
+            myCalendar += "END:VCALENDAR";
+            // console.log(myCalendar);
 
-        
-        const blob = new Blob([myCalendar], { type: "text/calendar;charset=utf-8" });
-        const url = URL.createObjectURL(blob);
-        const link = document.createElement("a");
-        link.href = url;
-        link.download = fileName;
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
-        URL.revokeObjectURL(url);
+            
+            const blob = new Blob([myCalendar], { type: "text/calendar;charset=utf-8" });
+            const url = URL.createObjectURL(blob);
+            const link = document.createElement("a");
+            link.href = url;
+            link.download = fileName;
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+            URL.revokeObjectURL(url);
+        }catch(error){
+            alert(error);
+        };
     }
     const CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID;
     const SCOPES = "https://www.googleapis.com/auth/calendar";
@@ -145,7 +148,7 @@ function App() {
     };
     const createNewCalendarAndEvent = async () => {
       if (!accessToken) return alert("Vui lòng kết nối Google trước!");
-  
+      setIsLoading(true);
       try {
         const splited = handleSplit();
         const lines = splited.lines;
@@ -196,7 +199,7 @@ function App() {
                     ]
                   };
 
-                console.log(event);
+                // console.log(event);
                 
                 const addEventRes = await fetch(`https://www.googleapis.com/calendar/v3/calendars/${newCalendarId}/events`, {
                 method: 'POST',
@@ -233,7 +236,7 @@ function App() {
                         `RRULE:FREQ=WEEKLY;UNTIL=${endDate.replace(/-/g, "")}T165959Z;BYDAY=${mapping[experiment[0]] || "MO"}`
                     ]
                 };
-                console.log(event);
+                // console.log(event);
 
                 const addEventRes = await fetch(`https://www.googleapis.com/calendar/v3/calendars/${newCalendarId}/events`, {
                 method: 'POST',
@@ -246,24 +249,25 @@ function App() {
                 await delay(2000);
                 if(check) check = addEventRes.ok;
 
-                console.log(event);
+                // console.log(event);
             }
 
             startIdx++;
         }
-
+        
         if(check)
           alert("Đã thêm lịch");
         else 
-          alert("Lỗi");
+          alert("Thêm lịch không thành công");
       } catch (error) {
-        alert("Lỗi: " + error);
+        alert(error);
+      } finally {
+        setIsLoading(false);
       }
     };
     return (
         <>
         <div className="flex h-screen items-center justify-center bg-gray-100 gap-6">
-            
             <div className="w-[400px] bg-white p-4 shadow-lg rounded-lg">
                 <div className="flex flex-col gap-2">
                     <label className="font-bold">Chọn ngày bắt đầu học kỳ:</label>
@@ -291,41 +295,35 @@ function App() {
                     value={content}
                     onChange={(e) => setContent(e.target.value)} 
                     className="w-full h-32 p-3 border border-gray-300 rounded-md focus:ring-2 focus:ring-blue-500 outline-none"
-                    placeholder="Hãy nhập Thời khóa biểu vào đây..."
+                    placeholder="Ctrl+A và dán toàn bộ thời khóa biểu vào đây"
                 ></textarea>
                 
                 <button 
                     className="mt-3 w-full bg-blue-600 text-white py-2 rounded-md hover:bg-blue-700 transition" 
                     onClick={createCalendarToFile}
                 >
-                    Lưu thời khóa biểu
+                    Tải xuống thời khóa biểu
                 </button>
-                <div style={{ padding: '20px', textAlign: 'center' }}>
+                <div className='p-[20px] items-center'>
                     <p>Trạng thái: <strong>{status}</strong></p>
                     
                     {!accessToken ? (
-                        <button onClick={handleConnect} style={btnStyle}>
-                        Kết nối Google
+                        <button className="w-full px-[20px] py-[10px] text-[16px] cursor-pointer bg-[#4285F4] text-white border-none rounded-[5px] hover:bg-[#357ae8] transition-colors" onClick={handleConnect}>
+                            Kết nối Google
                         </button>
                     ) : (
-                        <button onClick={createNewCalendarAndEvent} style={{...btnStyle, backgroundColor: '#28a745'}}>
-                        Thêm Lịch Thi vào Google Calendar
+                        <button 
+                            className={`w-full px-[20px] py-[10px] text-[16px] ${isLoading ? "bg-gray-300 cursor-not-allowed" : "bg-[#28a745] cursor-pointer hover:bg-[#357ae8]"} text-white border-none rounded-[5px] transition-colors`}
+                            onClick={createNewCalendarAndEvent}
+                            disabled={isLoading}
+                        >
+                            {isLoading ? (<> Đang xử lý ... </>) : (<>Thêm vào <strong>Google Calendar</strong></>)}
                         </button>
                     )}
                 </div>  
             </div>
-              
         </div>
         </>
     );
 }
-const btnStyle = {
-    padding: '10px 20px',
-    fontSize: '16px',
-    cursor: 'pointer',
-    backgroundColor: '#4285F4',
-    color: 'white',
-    border: 'none',
-    borderRadius: '5px'
-  };
 export default App;
